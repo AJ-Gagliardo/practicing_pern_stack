180a515adbf1781cf86a8084764f1b53556d92e3
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-10-16 11:35:36 -0700

    made a delete button that deletes the task and then the state is updated to show it on the ui without refreshing

--- client/src/components/InputTodo.jsx
+++ client/src/components/InputTodo.jsx
@@ -21,9 +21,12 @@ const InputTodo = () => {
     }
   };
   return (
-    <>
+    <div className="flex flex-col justify-center place-items-center">
       <h1 className="text-center mt-5 flex flex-col">Input Todo</h1>
-      <form className="flex flex-col items-center" onSubmit={onSubmitForm}>
+      <form
+        className="flex flex-col justify-center place-items-center"
+        onSubmit={onSubmitForm}
+      >
         <input
           className="border border-zinc-800"
           type="text"
@@ -32,7 +35,7 @@ const InputTodo = () => {
         />
         <button className="bg-green-500 text-red-800 m-2 p-2">Add</button>
       </form>
-    </>
+    </div>
   );
 };
 
--- client/src/components/ListTodos.jsx
+++ client/src/components/ListTodos.jsx
@@ -1,24 +1,71 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function ListTodos() {
-  const getTodos = async () => {
-    try {
-      const response = await fetch("http://localhost:5000/todos");
-      const jsonData = await response.json();
+  const [todos, setTodos] = useState([]);
 
-      console.log(jsonData);
+  //delete todo function
+
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+      console.log(deleteTodo);
     } catch (err) {
       console.error(err.message);
     }
   };
 
+  // get todo function
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const jsonData = await response.json();
+
+      setTodos(jsonData);
+      // console.log(jsonData);
+      // console.log(todos);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  console.log(todos);
+
   useEffect(() => {
     getTodos();
-  });
+    // console.log(todos);
+  }, []);
+
+  // console.log(todos);
+  // console.log(todos);
 
   return (
-    <div>
+    <div className="flex flex-col justify-center place-items-center">
       <h1>List Todos</h1>
+      <>
+        {todos.map((todo) => (
+          <ul
+            className="grid grid-cols-10 sm:w-2/4  gap-x-4 gap-y-2 my-4 mx-2 text-start place-content-center justify-center justify-items-center"
+            key={todo.todo_id}
+          >
+            <li className="col-span-8 text-start">{todo.description}</li>
+            <li className="col-span-1">
+              <button>Edit</button>
+            </li>
+            <li className="col-span-1">
+              <button
+                className="bg-red-600"
+                onClick={() => deleteTodo(todo.todo_id)}
+              >
+                Delete
+              </button>
+            </li>
+          </ul>
+        ))}
+      </>
     </div>
   );
 }
